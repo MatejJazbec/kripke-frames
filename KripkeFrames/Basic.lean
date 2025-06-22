a82a803b5023bb86f32b0c82453b237b4f6d8890
MatejJazbec <jazbec.matej@outlook.com>
2025-06-22 23:58:26 +0200

    Various developments. Hyperreferences might not work yet.

--- KripkeFrames/Basic.lean
+++ KripkeFrames/Basic.lean
@@ -1,3 +1,7 @@
+import Mathlib.Tactic
+
+
+
 -- Definition of Kripke frame
 structure KripkeFrame where
   (W : Type)                 -- A type representing the set of possible worlds
@@ -12,30 +16,28 @@ def KripkeFrame_is_transitive (F : KripkeFrame) : Prop :=
 def KripkeFrame_is_symmetric (F : KripkeFrame) : Prop :=
   ∀ w v : F.W, F.R w v → F.R v w
 
+def KripkeFrame_is_serial (F : KripkeFrame) : Prop :=
+  ∀ w : F.W, ∃ v : F.W, F.R w v
 
+def KripkeFrame_is_dense (F : KripkeFrame) : Prop :=
+  ∀ w v : F.W, F.R w v → ∃ u : F.W, F.R w u ∧ F.R u v
+
+def KripkeFrame_is_euclidean (F : KripkeFrame) : Prop :=
+  ∀ w v u : F.W, F.R w v → F.R w u → F.R v u
+
+-- Definition of Kripke frames with R having specific property
 -- structure ReflexiveTransitiveKripkeFrame extends KripkeFrame :=
 --   (refl : ∀ w : W, R w w)                        -- Reflexivity
 --   (trans : ∀ w v u : W, R w v → R v u → R w u)   -- Transitivity
 
+-- Definition of Kripke model
 structure KripkeModel extends KripkeFrame where
   (V : W → Prop → Prop)  -- Valuation function mapping worlds to truth values of propositions
 
-inductive World
-| w1 | w2 | w3
 
-open World
-
-def example_frame : KripkeFrame :=
-{ W := World,
-  R := λ w₁ w₂ ↦
-    (w₁ = w1 ∧ w₂ = w2) ∨
-    (w₁ = w2 ∧ w₂ = w3) ∨
-    (w₁ = w3 ∧ w₂ = w1) }
-
--- #eval KripkeFrame_is_reflexive example_frame -- Should return `false` for the above example
-
+-- Definition of modalities
 def box (F : KripkeModel) (φ : F.W → Prop) (w : F.W) : Prop :=
-  ∀ v : F.W, F.R w v → φ v
+  ∀ w' : F.W, F.R w w' → φ w'
 
 def diamond (F : KripkeModel) (φ : F.W → Prop) (w : F.W) : Prop :=
-  ∃ v : F.W, F.R w v ∧ φ v
+  ∃ w' : F.W, F.R w w' ∧ φ w'
